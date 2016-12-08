71452115ee3aa3b0ac1ea1fcb1fba6bed1443592
jl2569 <jl2569@nau.edu>
2016-12-08 15:31:10 -0700

    added rotation

---
+++ README.md
@@ -0,0 +1,7 @@
+This project was done windows using the visual studios command line. To create the make file all that needs to be done is call nmake and it will create a ezview.exe which is called with one parameter(ppm file) to fully run the application. Currently the p6 type files do not work for reasons unknown.
+To transform the image you would need this key to help you out:
+Directional arrows translate the image ;
+W rotates the image clock-wise , while S rotates the image counter clock-wise;
+A scales the image closer , while D scales the image further away;
+Q shears the image to the left , while E shears the image to the right;
+ESC closes the window;
--- ezview.c
+++ ezview.c
@@ -8,6 +8,7 @@
 #include <assert.h>
 #include <string.h>
 #include <math.h>
+#include <linmath.h>
 #include <ctype.h>
 
 
@@ -19,6 +20,7 @@ typedef struct {
   float TexCoord[2];
 } Vertex;
 
+// header struct for the header of the image 
 typedef struct header{
 	char* magicnumber;
 	char* width;
@@ -27,7 +29,7 @@ typedef struct header{
 }header;
 
 header *head;
-
+// pic struct that hold each pixels color value.
 typedef struct pic {
   unsigned char r, g, b;
 } pic;
@@ -121,6 +123,7 @@ int simple_program() {
 static void error_callback(int error, const char* description) {
   fputs(description, stderr);
 }
+// skips the whitespace in an file so easier to parse
 void skip_ws(FILE* input_file) {
   int c = fgetc(input_file);
   while (isspace(c)) {
@@ -128,18 +131,18 @@ void skip_ws(FILE* input_file) {
   }
   ungetc(c, input_file);
 }
+// Function that reads the files header and add the right values to the struct
 void read_header(FILE* filee){
 	char temp[64];
 	int a = 0;
 	skip_ws(filee);
+	// records the magic number then copies it into struct
 	temp[a] = fgetc(filee);
 	a +=1;
-	temp[a] = fgetc(filee); 
-	
+	temp[a] = fgetc(filee); 	
 	a = 0; 
 	strcpy(head->magicnumber, temp); 
-	memset(temp, 0, 64);  
-	
+	// records the width then copies it into struct but takes into account if it runs into a comment
 	skip_ws(filee);
 	char c = fgetc(filee);
 	while(!isspace(c)){
@@ -153,17 +156,17 @@ void read_header(FILE* filee){
 	}	
 	a = 0; 
 	strcpy(head->width, temp); 
-
+	// records the height then copies it into struct
 	skip_ws(filee);
 	c = fgetc(filee);
 	while(!isspace(c)){
 		temp[a] = c; 
 		a +=1;
 		c = fgetc(filee);	
-	}
-	
+	}	
 	a =0; 
-	strcpy(head->height, temp); 		
+	strcpy(head->height, temp);
+	// records the color value then copies it into struct
 	skip_ws(filee);
 	c = fgetc(filee);
 	while(!isspace(c)){
@@ -174,13 +177,14 @@ void read_header(FILE* filee){
 	strcpy(head->colorval, temp); 	
 	skip_ws(filee);
 }
-
+//Reads the rest of the file recording the color of each single pixel and records it into the struct
 void read_file(FILE* filee  ){
 	int place;
     int size = atoi(head->width) * atoi(head->height);
     int r;
     int g;
     int b;
+	// Follows the if statement solely dependent on what the magic number is if it is no p3 or p6 then error
 	if ((strcmp(head->magicnumber, "P3") ==0) || (strcmp(head->magicnumber, "p3") ==0) ){
 		for(int i = 0; i < size; i++){
 			place = fgetc(filee);
@@ -191,7 +195,7 @@ void read_file(FILE* filee  ){
 			fscanf(filee, "%d %d %d", &r, &g, &b);
 			if (r > atoi(head->colorval) || g > atoi(head->colorval) || b > atoi(head->colorval) || r < 0 || g < 0 || b < 0){
 				printf("Error: Number values are less than 0 or greater than colorvalue");
-				return(1);
+				exit(1);
 			}else{
 				render[i].r = r;
 				render[i].g = g;
@@ -205,30 +209,35 @@ void read_file(FILE* filee  ){
 			fread(&render[i].b, 1, 1, filee);
 			if (render[i].r > atoi(head->colorval) || render[i].g > atoi(head->colorval) || render[i].b > atoi(head->colorval) || render[i].r < 0 || render[i].g < 0 || render[i].b < 0){
 				printf("Error: Number values are less than 0 or greater than colorvalue");
-				return(1);
+				exit(1);
 			}
 		}
 	}else{
 		printf("Error: On supports p3 and p6 image files\n");
-		return(1);
+		exit(1);
 	}
 }
+// Function that translates the image 
 void move(int movement){
+	// Moves image up
 	if (movement == 0 ){
 		Vertices[0].position[1] += .15;
         Vertices[1].position[1] += .15;
         Vertices[2].position[1] += .15;
         Vertices[3].position[1] += .15;
+	// Moves image down
 	}else if (movement == 1 ){
 		Vertices[0].position[1] -= .15;
         Vertices[1].position[1] -= .15;
         Vertices[2].position[1] -= .15;
         Vertices[3].position[1] -= .15;
+	// Moves image left
 	}else if (movement == 2 ){
 		Vertices[0].position[0] -= .15;
         Vertices[1].position[0] -= .15;
         Vertices[2].position[0] -= .15;
         Vertices[3].position[0] -= .15;
+	// Moves image right
 	}else if (movement == 3 ){
 		Vertices[0].position[0] += .15;
         Vertices[1].position[0] += .15;
@@ -238,6 +247,7 @@ void move(int movement){
 
     
 }
+// Function that shears the image 
 void shear(int movement){
 	if (movement == 0){
 		Vertices[0].position[0] += 0.1;
@@ -252,10 +262,55 @@ void shear(int movement){
 	}
     
 }
+float turn = 0;
+// Function that rotates the image
 void rotate(int movement){
-	printf("hi");
+	// rotates 30 degrees
+	float x;
+	float y;
+	// rotate clock-wise
+	if (movement == 0 ){
+		float mat[2][2] = {{sqrt(1)/2,-sqrt(3)/2},{sqrt(3)/2,sqrt(1)/2}};
+		x = (Vertices[0].position[0]*mat[0][0]) + (Vertices[0].position[1]*mat[1][0]);
+		y = (Vertices[0].position[0]*mat[0][1]) + (Vertices[0].position[1]*mat[1][1]);
+		Vertices[0].position[0] = x;
+		Vertices[0].position[1] = y;
+		x = (Vertices[1].position[0]*mat[0][0]) + (Vertices[1].position[1]*mat[1][0]);
+		y = (Vertices[1].position[0]*mat[0][1]) + (Vertices[1].position[1]*mat[1][1]);
+		Vertices[1].position[0] = x;
+		Vertices[1].position[1] = y;
+		x = (Vertices[2].position[0]*mat[0][0]) + (Vertices[2].position[1]*mat[1][0]);
+		y = (Vertices[2].position[0]*mat[0][1]) + (Vertices[2].position[1]*mat[1][1]);
+		Vertices[2].position[0] = x;
+		Vertices[2].position[1] = y;
+		x = (Vertices[3].position[0]*mat[0][0]) + (Vertices[3].position[1]*mat[1][0]);
+		y = (Vertices[3].position[0]*mat[0][1]) + (Vertices[3].position[1]*mat[1][1]);
+		Vertices[3].position[0] = x;
+		Vertices[3].position[1] = y;
+	// roatate counter-clock-wise
+	}else if (movement == 1){
+		float tam[2][2] = {{sqrt(1)/2,sqrt(3)/2},{-sqrt(3)/2,sqrt(1)/2}};
+		x = (Vertices[0].position[0]*tam[0][0]) + (Vertices[0].position[1]*tam[1][0]);
+		y = (Vertices[0].position[0]*tam[0][1]) + (Vertices[0].position[1]*tam[1][1]);
+		Vertices[0].position[0] = x;
+		Vertices[0].position[1] = y;
+		x = (Vertices[1].position[0]*tam[0][0]) + (Vertices[1].position[1]*tam[1][0]);
+		y = (Vertices[1].position[0]*tam[0][1]) + (Vertices[1].position[1]*tam[1][1]);
+		Vertices[1].position[0] = x;
+		Vertices[1].position[1] = y;
+		x = (Vertices[2].position[0]*tam[0][0]) + (Vertices[2].position[1]*tam[1][0]);
+		y = (Vertices[2].position[0]*tam[0][1]) + (Vertices[2].position[1]*tam[1][1]);
+		Vertices[2].position[0] = x;
+		Vertices[2].position[1] = y;
+		x = (Vertices[3].position[0]*tam[0][0]) + (Vertices[3].position[1]*tam[1][0]);
+		y = (Vertices[3].position[0]*tam[0][1]) + (Vertices[3].position[1]*tam[1][1]);
+		Vertices[3].position[0] = x;
+		Vertices[3].position[1] = y;
+	}
 }
+// Function that scales the image
 void scale(int movement){
+	// zooms the image in
 	if (movement == 0){
 		Vertices[0].position[0] *= 1.1;
         Vertices[0].position[1] *= 1.1;
@@ -265,6 +320,7 @@ void scale(int movement){
         Vertices[2].position[1] *= 1.1;
         Vertices[3].position[0] *= 1.1;
         Vertices[3].position[1] *= 1.1;
+	// zooms the image out
 	}else if (movement == 1){
 		Vertices[0].position[0] *= .9;
         Vertices[0].position[1] *= .9;
@@ -277,9 +333,10 @@ void scale(int movement){
 	}
     
 }
-
+// Function that sets up the keys for the transformations of the image
 void keys(GLFWwindow* window, int key, int scancode, int action, int mods){
-    if (key == GLFW_KEY_UP && action == GLFW_PRESS){
+    //move
+	if (key == GLFW_KEY_UP && action == GLFW_PRESS){
         move(0);
 	}else if (key == GLFW_KEY_DOWN && action == GLFW_PRESS){
         move(1);
@@ -287,18 +344,22 @@ void keys(GLFWwindow* window, int key, int scancode, int action, int mods){
         move(2);
 	}else if (key == GLFW_KEY_RIGHT && action == GLFW_PRESS){
         move(3);
+	//rotate
 	}else if (key == GLFW_KEY_W && action == GLFW_PRESS){
         rotate(0);
 	}else if (key == GLFW_KEY_S && action == GLFW_PRESS){
         rotate(1);
+	//scale
 	}else if (key == GLFW_KEY_A && action == GLFW_PRESS){
         scale(0);
 	}else if (key == GLFW_KEY_D && action == GLFW_PRESS){
         scale(1);
+	//shear
 	}else if (key == GLFW_KEY_Q && action == GLFW_PRESS){
         shear(0);
 	}else if (key == GLFW_KEY_E && action == GLFW_PRESS){
         shear(1);
+	//closes the window
 	}else if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS){
 		glfwSetWindowShouldClose(window, 1);
 	}
@@ -324,7 +385,6 @@ int main(int argc , char *argv[]) {
 	head->colorval = (char *)malloc(100);
 
 	read_header(fh);
-	
     render = (pic *)malloc(sizeof(pic) * atoi(head->width) * atoi(head->height)  + 1);
 	read_file(fh );
 	fclose(fh);
