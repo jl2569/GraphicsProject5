e4b86bb2dbaa676cbd9b207894a53c77ce3541b9
jl2569 <jl2569@nau.edu>
2016-12-08 06:53:47 -0700

    rendered image

--- demo.c
+++ demo.c
@@ -3,9 +3,12 @@
 #define GL_GLEXT_PROTOTYPES
 #include <GLES2/gl2.h>
 #include <GLFW/glfw3.h>
-
 #include <stdlib.h>
 #include <stdio.h>
+#include <assert.h>
+#include <string.h>
+#include <math.h>
+#include <ctype.h>
 
 
 GLFWwindow* window;
@@ -13,15 +16,20 @@ GLFWwindow* window;
 
 typedef struct {
   float position[3];
-  float color[4];
+  float TexCoord[2];
 } Vertex;
 
+typedef struct pic {
+  unsigned char r, g, b;
+} pic;
+
+pic *render;
 
 const Vertex Vertices[] = {
-  {{1, -1, 0}, {1, 0, 0, 1}},
-  {{1, 1, 0}, {0, 1, 0, 1}},
-  {{-1, 1, 0}, {0, 0, 1, 1}},
-  {{-1, -1, 0}, {0, 0, 0, 1}}
+  {{1, -1, 0},  {.9999, .9999}},
+  {{1, 1, 0},   {.9999, 0}},
+  {{-1, 1, 0},  {0, 0}},
+  {{-1, -1, 0}, {0, .9999}}
 };
 
 
@@ -33,21 +41,22 @@ const GLubyte Indices[] = {
 
 char* vertex_shader_src =
   "attribute vec4 Position;\n"
-  "attribute vec4 SourceColor;\n"
   "\n"
-  "varying vec4 DestinationColor;\n"
+  "attribute vec2 TexCoordIn;\n"
+  "varying lowp vec2 TexCoordOut;\n"
   "\n"
   "void main(void) {\n"
-  "    DestinationColor = SourceColor;\n"
+  "    TexCoordOut = TexCoordIn;\n"
   "    gl_Position = Position;\n"
   "}\n";
 
 
 char* fragment_shader_src =
-  "varying lowp vec4 DestinationColor;\n"
+  "varying lowp vec2 TexCoordOut;\n"
+  "uniform sampler2D Texture;\n"
   "\n"
   "void main(void) {\n"
-  "    gl_FragColor = DestinationColor;\n"
+  "    gl_FragColor = texture2D(Texture, TexCoordOut);\n"
   "}\n";
 
 
@@ -81,7 +90,7 @@ int simple_program() {
   GLint program_id = glCreateProgram();
   GLint vertex_shader = simple_shader(GL_VERTEX_SHADER, vertex_shader_src);
   GLint fragment_shader = simple_shader(GL_FRAGMENT_SHADER, fragment_shader_src);
-
+  
   glAttachShader(program_id, vertex_shader);
   glAttachShader(program_id, fragment_shader);
 
@@ -103,6 +112,34 @@ int simple_program() {
 static void error_callback(int error, const char* description) {
   fputs(description, stderr);
 }
+void skip_ws(FILE* input_file) {
+  int c = fgetc(input_file);
+  while (isspace(c)) {
+    c = fgetc(input_file);
+  }
+  ungetc(c, input_file);
+}
+
+void read_file(FILE* filee , char* magicnumber , char* width , char* height ){
+	
+	int curr;
+    int size = atoi(width) * atoi(height);
+    int r;
+    int g;
+    int b;
+    for(int i = 0; i < size; i++){
+
+      curr = fgetc(filee);
+      while(curr  == ' ' || curr  == '\n'){
+          curr = fgetc(filee);
+      }
+      ungetc(curr, filee);
+      fscanf(filee, "%d %d %d", &r, &g, &b);
+      render[i].r = r;
+      render[i].g = g;
+      render[i].b = b;
+    }
+}
 
 
 int main(int argc , char *argv[]) {
@@ -119,26 +156,26 @@ int main(int argc , char *argv[]) {
 		perror("Error: Couldn't open file.");
 		return(-1);
 	}
-	char* magicnumber = malloc(500);
-	char* height = malloc(500);
-	char* weight = malloc(500);
-	char* colorval = malloc(500);
+	char* magicnumber = malloc(100);
+	char* height = malloc(100);
+	char* width = malloc(100);
+	char* colorval = malloc(100);
 
 	magicnumber = fgets(magicnumber,3,fh);
-	char rah = fgetc(fh);
+	skip_ws(fh);
+	width= fgets(width,4,fh);
+	skip_ws(fh);
 	height = fgets(height,4,fh);
-	char ah = fgetc(fh);
-	weight= fgets(weight,4,fh);
-	char mah = fgetc(fh);
+	skip_ws(fh);
 	colorval= fgets(colorval,4,fh);
 	
-	
-	fclose(fh);
-	return(0);
-
-  GLint program_id, position_slot, color_slot;
-  GLuint vertex_buffer;
-  GLuint index_buffer;
+       render = (pic *)malloc(sizeof(pic) * atoi(width) * atoi(height)  + 1);
+	   read_file(fh , magicnumber ,width,height);
+	   fclose(fh);
+	   
+	GLint program_id, position_slot;
+	GLuint vertex_buffer;
+	GLuint index_buffer;
 
   glfwSetErrorCallback(error_callback);
 
@@ -153,72 +190,100 @@ int main(int argc , char *argv[]) {
   glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
 
   // Create and open a window
-  window = glfwCreateWindow(640,
-                            480,
-                            "Hello World",
+  window = glfwCreateWindow(atoi(width),
+                            atoi(height),
+                            argv[1],
                             NULL,
                             NULL);
-
   if (!window) {
     glfwTerminate();
     printf("glfwCreateWindow Error\n");
     exit(1);
   }
-
+ 
   glfwMakeContextCurrent(window);
+  
+  
+	GLuint texID;
+    glGenTextures(1, &texID);
+    glBindTexture(GL_TEXTURE_2D, texID);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, atoi(width), atoi(height), 0, GL_RGB, 
+				 GL_UNSIGNED_BYTE, render);
+	//
 
-  program_id = simple_program();
+				 
+ 	program_id = simple_program();
 
-  glUseProgram(program_id);
+	glUseProgram(program_id);
 
-  position_slot = glGetAttribLocation(program_id, "Position");
-  color_slot = glGetAttribLocation(program_id, "SourceColor");
-  glEnableVertexAttribArray(position_slot);
-  glEnableVertexAttribArray(color_slot);
+	// sets position_slot and asserts it was set correctly, followed by calling glEnableVertexAttribArray on it
+	position_slot = glGetAttribLocation(program_id, "Position");
+	assert(position_slot != -1);
+	glEnableVertexAttribArray(position_slot);
+	
+	// sets texcoord_location and asserts it was set correctly
+	GLint texcoord_location = glGetAttribLocation(program_id, "TexCoordIn");
+	assert(texcoord_location != -1);
+	
+	// sets tex_location and asserts it was set correctly
+	GLint tex_location = glGetUniformLocation(program_id, "Texture");
+	assert(tex_location != -1);
+	
+	// calls glEnableVertexAtribArray on previously set texcoord_location, now that we know the tex_location was set properly
+	glEnableVertexAttribArray(texcoord_location);
 
-  // Create Buffer
-  glGenBuffers(1, &vertex_buffer);
+	// Create Buffer
+	glGenBuffers(1, &vertex_buffer);
 
-  // Map GL_ARRAY_BUFFER to this buffer
-  glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer);
+	// Map GL_ARRAY_BUFFER to this buffer
+	glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer);
 
-  // Send the data
-  glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices), Vertices, GL_STATIC_DRAW);
+	// Send the data
+	glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices), Vertices, GL_STATIC_DRAW);
 
-  glGenBuffers(1, &index_buffer);
-  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, index_buffer);
-  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(Indices), Indices, GL_STATIC_DRAW);
+	glGenBuffers(1, &index_buffer);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, index_buffer);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(Indices), Indices, GL_STATIC_DRAW);
 
   // Repeat
   while (!glfwWindowShouldClose(window)) {
+	  
+glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices), Vertices, GL_STATIC_DRAW); // updates image if any transformations were performed
 
-    glClearColor(0, 104.0/255.0, 55.0/255.0, 1.0);
-    glClear(GL_COLOR_BUFFER_BIT);
+		glClearColor(0, 0, 0, 0);
+		glClear(GL_COLOR_BUFFER_BIT);
 
-    glViewport(0, 0, 640, 480);
+		glViewport(0, 0, atoi(width), atoi(height));
 
-    glVertexAttribPointer(position_slot,
-                          3,
-                          GL_FLOAT,
-                          GL_FALSE,
-                          sizeof(Vertex),
-                          0);
+		glVertexAttribPointer(position_slot,
+							  3,
+							  GL_FLOAT,
+							  GL_FALSE,
+							  sizeof(Vertex),
+							  0);		  
+		glVertexAttribPointer(texcoord_location,
+							  2,
+							  GL_FLOAT,
+							  GL_FALSE,
+							  sizeof(Vertex),
+							  (void*) (sizeof(float) * 3)); // modify last parameter? * 7 due to offset
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, texID);
+		glUniform1i(tex_location, 0);
 
-    glVertexAttribPointer(color_slot,
-                          4,
-                          GL_FLOAT,
-                          GL_FALSE,
-                          sizeof(Vertex),
-                          (GLvoid*) (sizeof(float) * 3));
+		
+		glDrawElements(GL_TRIANGLES,
+					   sizeof(Indices) / sizeof(GLubyte),
+					   GL_UNSIGNED_BYTE, 0);
 
-    glDrawElements(GL_TRIANGLES,
-                   sizeof(Indices) / sizeof(GLubyte),
-                   GL_UNSIGNED_BYTE, 0);
-
-    glfwSwapBuffers(window);
-    glfwPollEvents();
+		glfwSwapBuffers(window);
+		glfwPollEvents();
   }
 
+  //fclose(fh);
+	
   glfwDestroyWindow(window);
   glfwTerminate();
   exit(EXIT_SUCCESS);
